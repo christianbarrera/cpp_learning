e95897596905354b17690b946da82093faae3a02
christianbarrera <christian.michael.barrera@gmail.com>
2017-12-21 01:02:50 -0600

    Initial Commit

---
+++ test_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test_project
+//
+//  Created by Christian Barrera on 12/21/17.
+//  Copyright © 2017 Christian Barrera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
