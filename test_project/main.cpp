49e91b66e21786e0a302a5080e9e9d35d21488ad
christianbarrera <christian.michael.barrera@gmail.com>
2017-12-25 21:23:38 -0600

    Entering the code for the while/for loop practice.

--- test_project/main.cpp
+++ test_project/main.cpp
@@ -8,8 +8,23 @@
 
 #include <iostream>
 
-int main() {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    int input;
+    
+    while(true) {
+        cin >> input;
+        if (input > 10) {
+            break;
+        }   else if (abs(input) % 2 == 1) {
+            cout<<(input/3+1)*3;
+        }   else {
+            for (int a = 0; a < input; a++ ) {
+                cout<< "Hello";
+            }
+        }
+    }
     return 0;
 }
